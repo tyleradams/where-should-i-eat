36335476b1302e8ab97fd2c434aef2bb35d26bd1
Tyler Adams <adams@squareup.com>
2015-12-11 14:59:14 -0500

    Adding restaurants, naive weighting

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,13 @@
 class WelcomeController < ApplicationController
 
+  RESTAURANTS = [
+    "https://foursquare.com/v/egg-shop/53f0c6bc498e01d5d2106388",
+    "https://foursquare.com/v/sunrise-mart/43dd08c1f964a520a52e1fe3",
+    "https://foursquare.com/v/olives/4a894957f964a520d80720e3",
+    "https://foursquare.com/v/souk--sandwich/5500b90a498ee31c16cc65b7",
+    "https://foursquare.com/v/despa%C3%B1a/45697387f964a520e53d1fe3"
+  ]
+
   def index
     pick_restaurant
   end
@@ -7,10 +15,6 @@ class WelcomeController < ApplicationController
   private
 
   def pick_restaurant
-    @url = randomly_picked_restaurant
-  end
-
-  def randomly_picked_restaurant
-    "https://foursquare.com/v/egg-shop/53f0c6bc498e01d5d2106388"
+    @url = RESTAURANTS.sample
   end
 end
